26d25604ecf526829e12e9b273075edf729a7981
https://github.com/sifathossen48 <sifathossen967@gmail.com>
2024-05-14 16:35:51 +0600

    Static_Root add

--- Believer_Company_Website/settings.py
+++ Believer_Company_Website/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = "www/believercompliance.com/.com/static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
