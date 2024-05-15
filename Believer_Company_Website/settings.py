3d29f96435a14b73186ddfc0f4d296cbedc2491c
https://github.com/sifathossen48 <sifathossen967@gmail.com>
2024-05-15 11:33:13 +0600

    updated

--- Believer_Company_Website/settings.py
+++ Believer_Company_Website/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static/'
+    os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/asset/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
